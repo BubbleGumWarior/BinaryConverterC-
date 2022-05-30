723af13f18ca581391a296790a0c651c23b81c95
BubbleGumWarior <bubblegumwarior@gmail.com>
2022-05-30 15:35:03 +0200

    Final
    Final

---
+++ BinaryConverter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BinaryConverter
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int value = 10;
+            var binary = Convert.ToString(value, 2);
+ 
+            Console.WriteLine(binary);
+        }
+        public static string ToBinary(int x)
+        {
+            var character = new char[32];
+ 
+            for (var i = 31; i >= 0 ; i--)
+            {
+                var pos = 1 << i;
+                character[31 - i] = (x & pos) != 0 ? '1' : '0';
+            }
+            return new string(character);
+        }
+    }
+}
